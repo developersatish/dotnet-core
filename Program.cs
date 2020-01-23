b77d409e3936d2b6b82927a860b336cfdd88d723
Satish Singh <imsatish.net@gmail.com>
2020-01-23 10:50:21 +0530

    iniit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace core_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class A {
+        public A()
+        {
+            Console.WriteLine("A constructor");
+        }
+
+        static A(){
+            Console.WriteLine("Static A()");
+        }
+
+        public void Method(){
+            Console.WriteLine("In A method");
+        }
+    }
+
+    public class B:A {
+
+        public B()
+        {
+            Console.WriteLine("Contractor B");
+        }
+
+        static B(){
+            Console.WriteLine("B static");
+        }
+    }
+}
