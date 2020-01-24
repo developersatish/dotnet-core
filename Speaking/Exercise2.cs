7faff6e8b25975d78b62ee0ed6925be25636dd54
Satish Singh <satishsingh.net@outlook.com>
2020-01-24 23:35:58 +0530

    console formatting

--- Speaking/Exercise2.cs
+++ Speaking/Exercise2.cs
@@ -13,7 +13,47 @@ namespace core_playground
 
         public static void Entry()
         {
-            WriteLine($"size of sbyte: {sizeof(sbyte)} min size: {sbyte.MinValue} Max Size: {sbyte.MaxValue}");
+            //  FormatString();
+            BreakLine();
+            WriteLine("{0,-15} {1,-5} {2,30} {3,30}", "Type", "Bytes of memory", "Min", "Max");
+            BreakLine();
+            PrintFormat("sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+            PrintFormat("byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+            PrintFormat("byte", sizeof(short), short.MinValue, short.MaxValue);
+
+            PrintFormat("ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+            PrintFormat("int", sizeof(int), int.MinValue, int.MaxValue);
+            PrintFormat("uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+
+            PrintFormat("long", sizeof(long), long.MinValue, long.MaxValue);
+            PrintFormat("ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+            PrintFormat("float", sizeof(float), float.MinValue, float.MaxValue);
+
+
+            PrintFormat("double", sizeof(double), double.MinValue, double.MaxValue);
+            PrintFormat("decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+            BreakLine();
+        }
+
+        static void BreakLine()
+        {
+            WriteLine("------------------------------------------------------------------------------------------------------------");
+
+        }
+        static void PrintFormat(string name, dynamic size, dynamic min, dynamic max)
+        {
+            WriteLine("{0,-15} {1,-15} {2,30} {3,30}", name, size, min, max);
+        }
+        static void FormatString()
+        {
+            string[] names = { "Adam", "Bridgette", "Carla", "Daniel",
+                         "Ebenezer", "Francine", "George" };
+            decimal[] hours = { 40, 6.667m, 40.39m, 82, 40.333m, 80,
+                                 16.75m };
+
+            WriteLine("{0,-20} {1,5}\n", "Name", "Hours");
+            for (int ctr = 0; ctr < names.Length; ctr++)
+                WriteLine("{0,-15} {1,5:N1}", names[ctr], hours[ctr]);
         }
     }
 }
